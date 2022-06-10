d883d65ad68c12c528e6bc8d5cfd0da62d05fe9c
nurbaki <nurbakibayansal11@gmail.com>
2022-06-10 10:15:01 +0200

    guncelleme

--- Ödevler/guess my number/game.js
+++ Ödevler/guess my number/game.js
@@ -1,6 +1,10 @@
 
 
 let myNumber = ((Math.random()*100).toFixed(0)) % 21;
+
+// let myNumber = Math.ceil(Math.random()*20); Bu matematiksel olarak daha dogru
+
+
 console.log(myNumber);
 let hak = 10;
 let Score1 =10;
@@ -16,6 +20,8 @@ const score = document.querySelector(".score");
 const topScore = document.querySelector(".top-score");
 const mesage = document.querySelector(".msg");
 
+
+// tekrar tusuna basilirsa
 again.onclick=function() {
 
     myNumber = ((Math.random()*100).toFixed(0)) % 21;
@@ -33,17 +39,21 @@ again.onclick=function() {
 
 }
 
+//kontrol et butonuna basildiginda
+
 check.onclick=function() {
 
+    // oyundan sonra da tiklanmasi durumuna karsi, zaten kazanip kazanmadigini kontrol et
 if (win) {
 
-    hak = hak-1;
+    hak = hak-1; // hakkimiz 10. her tiklandiginda  1 dussun
     console.log(hak);
 
-    if (hak>=0) {
+    if (hak>=0) {  // hala hakkimiz kaldiysa
 
      Score1=hak;
         
+     // eger dogru tahmin edildiyse
      if (myNumber == guessInput.value) {
          mesage.innerHTML = "Tebrikler Kazandiniz";
          screen.innerHTML = myNumber;
@@ -55,7 +65,7 @@ if (win) {
              topScore.innerHTML = ScoreTop;
          }
      }
-
+    // eger yanlis tahmin edildi ve hala hakkimiz varsa
      if (myNumber != guessInput.value && hak > 0){
        if (myNumber < guessInput.value) {
         mesage.innerHTML = "AZALT";
@@ -65,15 +75,16 @@ if (win) {
        }
      }
     } else {
-        mesage.innerHTML = "Kaybettiniz. Tekrar Deneyin";
+        mesage.innerHTML = "Kaybettiniz. Tekrar Deneyin"; // hakkimiz kalmadiysa
     }
 
     score.innerHTML = Score1;
 
- } else mesage.innerHTML = "Tekrar'a tikla. Yeniden oyna!!";
+ } else mesage.innerHTML = "Tekrar'a tikla. Yeniden oyna!!"; // oyun bittigi halde, kontrol et e tiklanirsa
 
  }
 
+ // tiklamadan, enter a basarak oynamak icin
  guessInput.onkeydown=(tuslar)=>{
     //* eger 13 e tusuna  (enter )basildiysa
     if(tuslar.keyCode===13){
