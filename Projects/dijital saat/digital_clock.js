e3ced7e8cc58257391ac2c69fe153017959c0ab4
nurbaki <nurbakibayansal11@gmail.com>
2022-06-22 10:33:28 +0200

    fetch

---
+++ Projects/dijital saat/digital_clock.js
@@ -0,0 +1,76 @@
+//*=================================================================
+//*              Digital Clock
+//* ================================================================
+
+
+const sec = document.querySelector(".seconds");
+const minut = document.querySelector(".minutes");
+const Hour = document.querySelector(".hours");
+const PMAM = document.querySelector(".pm_am");
+
+let pm_am;
+let counter = new Date().getSeconds();
+let minutes = new Date().getMinutes();
+let hours = new Date().getHours();
+
+if (counter < 10) {
+    sec.innerHTML = `0${counter}`;
+}else sec.textContent = counter;
+
+if (minutes < 10) {
+    minut.innerHTML = `0${minutes}`;
+}else minut.innerHTML = minutes;
+
+if (hours < 10) {
+    Hour.innerHTML = `0${hours}`;
+}else Hour.innerHTML = hours%12;
+
+if (hours >=12 ) {
+    PMAM.innerHTML="PM";                    
+}else PMAM.innerHTML="AM";
+
+
+const Clock = setInterval(() => {
+    if (counter < 10) {
+        counter +=1;
+        sec.innerHTML = `0${counter}`;
+    } 
+    else if ( 9 < counter && counter <60) {
+        counter +=1;
+        sec.textContent = counter;
+
+    }
+    else if ( counter == 60) {
+        if (minutes < 10) {
+            minutes += 1;
+            minut.innerHTML = `0${minutes}`;
+        }
+        else if ( 9 <minutes && minutes <60) {
+            minutes += 1;
+            minut.innerHTML = minutes;
+        }
+        else if ( minutes == 60) {
+            if (hours >=12 ) {
+                PMAM.innerHTML="PM";                    
+            }else PMAM.innerHTML="AM";
+
+            if ((hours%12) < 10) {
+                hours += 1;                                 
+                Hour.innerHTML = `0${hours}`;
+            }
+            else if ( 9 < (hours%12) && (hours%12) < 12) {
+                hours += 1;
+                Hour.innerHTML = hours % 12;
+            }
+            else if ( (hours%12) == 0) {
+                Hour.innerHTML = `00`
+                hours=0;
+            }
+            minut.innerHTML = `00`;
+            minutes = 0;
+        }    
+    counter = 0;
+    sec.innerHTML = `00`;
+    }; 
+
+}, 1);
